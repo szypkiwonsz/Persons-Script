632dc96f55d60a0a058d325bf69b5623a0d4b340
szypkiwonsz <kacpersawicki321@gmail.com>
2020-08-06 19:35:25 +0200

    Add uploading the number of days until a person's birthday to the database
    - Add function to calculate the days to a person's birthday
    - Add function for data replacement to also include the days until the person's birthday (automatically when the object is created)
    - Add function to check if the day exists (February 29 in leap year)
    - Add field 'days_to_birthday' to the insert_to_database function to insert new data to the database

--- script/data_loader.py
+++ script/data_loader.py
@@ -1,3 +1,6 @@
+import calendar
+from datetime import datetime
+
 import models
 from database import Database
 
@@ -14,6 +17,40 @@ class DataLoader(Database):
         super().__init__(db_path)
         self.data = data
         self.add_database_field(models.Dob, 'dob', 'days_to_birthday', models.IntegerField(null=True))
+        self.add_days_to_birthday_to_data()
+
+    def add_days_to_birthday_to_data(self):
+        new_list = []
+        for person in self.data:
+            date_of_birth = datetime.strptime(person['dob']['date'], "%Y-%m-%dT%H:%M:%S.%f%z").date()
+            days_to_birthday = self.calculate_days_to_birthday(date_of_birth)
+            person['dob']['days_to_birthday'] = days_to_birthday
+            new_list.append(person)
+        self.data = new_list
+        return new_list
+
+    def calculate_days_to_birthday(self, date_of_birth):
+        """
+        :param date_of_birth: <datetime.date>, date of birth of the person
+        :return: <int>, days to a person's birthday
+        """
+        today = datetime.today()
+        delta1 = datetime(today.year, date_of_birth.month, date_of_birth.day)
+        if self.day_not_exist(today.year + 1, date_of_birth.month, date_of_birth.day):
+            delta2 = datetime(today.year + 1, date_of_birth.month, date_of_birth.day - 1)
+        else:
+            delta2 = datetime(today.year + 1, date_of_birth.month, date_of_birth.day)
+        if (delta1 - today).days > 0:
+            delta = delta1
+        else:
+            delta = delta2
+        days = (delta - today).days
+        return days
+
+    @staticmethod
+    def day_not_exist(year, month, day):
+        if not calendar.isleap(year) and month == 2 and day == 29:
+            return True
 
     def insert_to_database(self):
         with self.db.atomic():
@@ -55,7 +92,8 @@ class DataLoader(Database):
                 )
                 login_obj.save()
                 dob_obj = models.Dob(
-                    person=person_obj, date=person['dob']['date'], age=person['dob']['age']
+                    person=person_obj, date=person['dob']['date'], age=person['dob']['age'],
+                    days_to_birthday=person['dob']['days_to_birthday']
                 )
                 dob_obj.save()
                 registered_obj = models.Registered(
