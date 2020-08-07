c475c148f6328cd49552f0d1719f80abca790532
szypkiwonsz <kacpersawicki321@gmail.com>
2020-08-07 13:53:24 +0200

    Add argument that retrieves data from api and improved code formatting
    - Add get_api_url function to get url with amount of people to get from api
    - Move parser to create_parser function
    - Add load_data_api_check_argument to check if argument is correctly typed
    - Main code has been moved to main function
    - Add operations to be performed after entering the
    '-load-data-api' argument

--- script/script.py
+++ script/script.py
@@ -1,21 +1,40 @@
 import argparse
 import os
 
-from data_getter import DataFromFile
+from data_getter import DataFromFile, DataFromApi
 from data_loader import DataLoader
+
+DATABASE = 'persons.db'
+FILE = 'persons.json'
 
 
 def database_file_exist(db_path):
     return os.path.exists(db_path)
 
 
-parser = argparse.ArgumentParser(description='Human data operations')
-args = parser.parse_args()
+def get_api_url(number_of_people):
+    return f'https://randomuser.me/api/?results={number_of_people}'
 
-if __name__ == '__main__':
 
-    DATABASE = 'persons.db'
-    FILE = 'persons.json'
+def load_data_api_check_argument(x):
+    x = int(x)
+    if 0 > x <= 5000:
+        raise argparse.ArgumentTypeError('Argument has to be an int from 0 to 5000')
+    return x
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='Human data operations')
+    parser.add_argument(
+        '-load-data-api', help='inserts data from API to the database. You have to specify number of people to add as '
+                               'an argument (from 1 to 5000)', metavar='N', type=load_data_api_check_argument
+    )
+    return parser
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
 
     # If the database has not been created before, create it and insert the data from the json file
     if not database_file_exist(DATABASE):
@@ -26,3 +45,13 @@ if __name__ == '__main__':
 
     if not any(vars(args).values()):
         print('There are no arguments passed!')
+    elif args.load_data_api:
+        api_url = get_api_url(args.load_data_api)
+        api = DataFromApi(api_url)
+        persons_data = api.get_persons_data()
+        loader = DataLoader(DATABASE, persons_data)
+        loader.insert_to_database()
+
+
+if __name__ == '__main__':
+    main()
