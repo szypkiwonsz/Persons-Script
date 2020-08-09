77f64edb1f8679a6955da6ebb436d1d11bb8ed60
szypkiwonsz <kacpersawicki321@gmail.com>
2020-08-09 18:45:42 +0200

    Move fixtures to conftest file and move functions to test_utils

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -1,44 +1,10 @@
-from datetime import datetime
-
-import pytest
-
 import models
-from data_getter import DataFromFile
-from data_loader import DataLoader
-
-
-@pytest.fixture()
-def loader():
-    file = DataFromFile('./persons.json')
-    data = file.get_persons_data()
-    loader = DataLoader(':memory:', data)
-    return loader
 
 
 def test_add_days_to_birthday_to_data(loader):
     new_data_list = loader.add_days_to_birthday_to_data()
     for data in new_data_list:
         assert 'days_to_birthday' in data['dob']
-
-
-# def test_calculate_days_to_birthday(loader):
-#     today = datetime(datetime.today().year, datetime.today().month, datetime.today().day).date()
-#     yesterday = datetime(datetime.today().year, datetime.today().month, datetime.today().day - 1).date()
-#     tomorrow = datetime(datetime.today().year, datetime.today().month, datetime.today().day + 1).date()
-#     dates = [
-#         today, yesterday, tomorrow
-#     ]
-#     correctly_days = [
-#         365, 364, 1
-#     ]
-#     for date, correctly_day in zip(dates, correctly_days):
-#         days_to_birthday = loader.calculate_days_to_birthday(date)
-#         assert days_to_birthday == correctly_day
-
-
-# def test_day_not_exist(loader):
-#     assert loader.day_not_exist(1996, 2, 29) is None
-#     assert loader.day_not_exist(2021, 2, 29) == True
 
 
 def test_add_cleaned_phone_number_to_data(loader):
