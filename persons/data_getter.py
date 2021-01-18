b3d9eecb9329aa25c6bd032c12211c51a0e1e632
szypkiwonsz <kacpersawicki321@gmail.com>
2021-01-18 22:06:40 +0100

    Add comments

--- persons/data_getter.py
+++ persons/data_getter.py
@@ -2,9 +2,15 @@ import requests
 
 
 class Api:
+    """Class storing method to get data from api."""
 
     @staticmethod
     def get(url):
+        """
+        Gets json data from api.
+        :param url: <string> -> api url
+        :return: <dict> -> json data from api
+        """
         response = requests.get(url)
         if response.status_code != 200:
             raise requests.HTTPError(f"{response.status_code}")
