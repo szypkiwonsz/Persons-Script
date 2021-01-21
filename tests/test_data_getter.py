b794a8ef73a14ceb480e1766b1935f540737a2c3
szypkiwonsz <kacpersawicki321@gmail.com>
2021-01-21 21:43:37 +0100

    Fix function names

--- tests/test_data_getter.py
+++ tests/test_data_getter.py
@@ -5,14 +5,14 @@ from data_getter import Api
 
 
 @pytest.mark.api
-def test_api_get_200(requests_mock):
+def test_get_200(requests_mock):
     requests_mock.get(f'https://randomuser.me/api/?results={str(1)}', json={'name': 'awesome-mock'})
     response = Api.get(f'https://randomuser.me/api/?results={str(1)}')
     assert response == {'name': 'awesome-mock'}
 
 
 @pytest.mark.api
-def test_api_get_404(requests_mock):
+def test_get_404(requests_mock):
     requests_mock.get(f'https://randomuser.me/api/?results={str(1)}', status_code=404)
     with pytest.raises(HTTPError):
         response = Api.get(f'https://randomuser.me/api/?results={str(1)}')
